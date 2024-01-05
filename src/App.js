c0e5ec43cb1fdc7a3e56a0ed4905d4098b7297e0
kekedezha <dezhac6@gmail.com>
2024-01-05 16:01:01 -0500

    using URL params for navigation

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ const appRouter = createBrowserRouter(
   createRoutesFromElements(
   <Route path="/" element={<Root />}>
     <Route index element={<HomePage />}/>
+    <Route path=":type" element={<HomePage />}/>
+    <Route path=":type/:id" element={<PetDetailsPage />}/>
   </Route>
   ));
 
